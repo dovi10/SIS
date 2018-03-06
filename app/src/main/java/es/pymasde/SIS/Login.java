4370b4c1082bce74783dbf2c34cd09d5204e3eae
Tomer <tptomerpeled@gmail.com>
2018-03-07 01:12:28 +0200

    fixed startup problem + login button works + changed app title and package + changed version

---
+++ app/src/main/java/es/pymasde/SIS/Login.java
@@ -0,0 +1,26 @@
+package es.pymasde.SIS;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends Activity {
+
+    Button Login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Login = (Button)findViewById(R.id.Login_Button);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,BlueTerm.class);
+                startActivity(i);
+            }
+        });
+    }
+}
